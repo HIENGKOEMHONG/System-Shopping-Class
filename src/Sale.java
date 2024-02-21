9d96c3b28b7aa29ddc2e0356ed0b9c8c2889dacb
PC <git@github.com:HIENGKOEMHONG/Test1.git>
2024-02-21 18:33:30 +0700

    Shopping Commit

--- src/Sale.java
+++ src/Sale.java
@@ -31,27 +31,6 @@ class Sale {
         return totalServiceExpense + totalProductExpense;
     }
 
-/*    public void displayInfo() {
-        System.out.println("-------------------------------------");
-        System.out.println("|           SALE INFORMATION        |");
-        System.out.println("-------------------------------------");
-        System.out.println("Customer Name: " + customer.getCustomerName());
-        System.out.println("Customer Type: " + customer.getCustomerType());
-        System.out.println("Date: " + date);
-        System.out.println("-------------------------------------");
-        System.out.println("|           EXPENSE DETAILS         |");
-        System.out.println("-------------------------------------");
-        System.out.printf("Service Expense: $%.2f\n", serviceExpense, serviceExpense - (serviceExpense * customer.getServiceMemberDiscount()));
-        System.out.printf("Product Expense: $%.2f\n", productExpense, productExpense - (productExpense * customer.getProductMemberDiscount()));
-        System.out.println("-------------------------------------");
-        System.out.println("|           TOTAL EXPENSE           |");
-        System.out.println("-------------------------------------");
-        System.out.printf("Total Service Expense: $%.2f\n", serviceExpense - (serviceExpense * customer.getServiceMemberDiscount()));
-        System.out.printf("Total Product Expense: $%.2f\n", productExpense - (productExpense * customer.getProductMemberDiscount()));
-        System.out.println("Total Expense: $" + getTotalExpense());
-    }
-}*/
-
     public void displayInfo() {
         double serviceDiscount = customer.getServiceMemberDiscount();
         double productDiscount = customer.getProductMemberDiscount();
